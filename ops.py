3eee1a963c5c2ea3485a8db483468cb48f16a0dc
ChaitanyaChillarge <chaitanyachillarge@sony.com>
2024-08-20 10:55:12 +0530

    first commit

---
+++ ops.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    # return x+y
+    print("x + y")
+
+def subtract(x,y):
+    # return x-y
+    print("x - y")
+
+def multiply(x,y):
+    # return x*y
+    print("x * y")
+
+def divide(x,y):
+    # return x/y
+    print("x / y")
