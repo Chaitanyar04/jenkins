af9ac49789d03994a254f7b65fed51cd555360d0
ChaitanyaChillarge <chaitanyachillarge@sony.com>
2024-08-20 12:08:32 +0530

    third commit

--- ops.py
+++ ops.py
@@ -1,16 +1,29 @@
-def add(x,y):
-    return x+y
-    # print("x + y")
+# def add(x,y):
+#     return x+y
+#     # print("x + y")
 
-def subtract(x,y):
-    return x-y
-    # print("x - y")
+# def subtract(x,y):
+#     return x-y
+#     # print("x - y")
 
-def multiply(x,y):
-    return x*y
-    # print("x * y")
+# def multiply(x,y):
+#     return x*y
+#     # print("x * y")
 
-def divide(x,y):
-    return x/y
-    # print("x / y")
+# def divide(x,y):
+#     return x/y
+#     # print("x / y")
 
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
--- test_ops.py
+++ test_ops.py
@@ -1,3 +1,17 @@
+# from ops import *
+
+# def test_add():
+#     assert add(3,3) == 6
+
+# def test_subtract():
+#     assert subtract(3, 3) == 0
+
+# def test_multiply():
+#     assert multiply(4, 3) == 12
+
+# def test_divide():
+#     assert divide(15,5) == 3
+    
 from ops import *
 
 def test_add():
